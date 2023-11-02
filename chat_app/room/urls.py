fa871e50b2ab1ca4c87561b63e88c88edfc84dc5
SeanPhilippi <sean.philippi@gmail.com>
2023-11-02 11:04:08 -0400

    view and url for individual rooms added, rooms.html fix, room.html started

--- chat_app/room/urls.py
+++ chat_app/room/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.rooms, name='rooms')
+    path('', views.rooms, name='rooms'),
+    path('<slug:slug>/', views.room, name='room')
 ]
