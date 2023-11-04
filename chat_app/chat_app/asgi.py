c522d04e0f34b26b7250e4403881130e6bafcfca
SeanPhilippi <sean.philippi@gmail.com>
2023-11-04 13:48:04 -0400

    asgi import fix, csrf_token added to message input form

--- chat_app/chat_app/asgi.py
+++ chat_app/chat_app/asgi.py
@@ -14,7 +14,7 @@ from django.core.asgi import get_asgi_application
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 
-import room.routing
+from ..room import routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chat_app.settings')
 
@@ -22,7 +22,7 @@ application = ProtocolTypeRouter({
     "http": get_asgi_application(),
     "websocket": AuthMiddlewareStack(
         URLRouter(
-            room.routing.websocket_urlpatterns
+            routing.websocket_urlpatterns
         )
     )
 })
