3c3b1a356e3679463afeee59671a50073bb731a0
SeanPhilippi <sean.philippi@gmail.com>
2023-11-04 08:00:38 -0400

    javascript for room messages added, fixed typo

--- chat_app/room/routing.py
+++ chat_app/room/routing.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import consumers
+
 websocket_urlpatterns = [
-    path('we/<str:room_name>/', consumers.ChatConsumer.as_asgi()),
+    path('ws/<str:room_name>/', consumers.ChatConsumer.as_asgi()),
 ]
